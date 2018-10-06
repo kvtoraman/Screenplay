7713313a6b6443e880b97645f17117cf4ee9223d
Kamil Veli Toraman <kvtoraman@gmail.com>
2018-10-06 21:32:23 +0900

    Remove unnecessary files

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This is a screenplay parser that extracts dialogues between characters. However 
 
 	This will generate "all_movies.json" file in the same folder.
     
-1. Copy all_movies.json to html_crawlers folder. Run "json parser.ipynb" through jupyter notebook. This will read "all_movies.json" and will create "all_name_script.txt". This new txt file has a movie name and a link to its script for each movie in the json file.
+1. Copy all_movies.json to html_crawlers folder. Run "json_parser.py" through jupyter notebook. This will read "all_movies.json" and will create "all_name_script.txt". This new txt file has a movie name and a link to its script for each movie in the json file.
 
 1. Run "html_list_parser.ipynb" . This will read "all_name_script.txt" and will generate "All_out.txt". This file has all the relevant dialogues from the movie scripts.
 
--- html_crawlers/json_parser.py
+++ html_crawlers/json_parser.py
@@ -1,7 +1,32 @@
+
+# coding: utf-8
+
 import json
-from pprint import pprint
+from bs4 import BeautifulSoup
+from urllib import request
 
-with open('movies.json') as data_file:    
+with open('all_movies.json') as data_file:    
     data = json.load(data_file)
+    
+with open('all_name_script.txt','w') as out:    
+    for movie in data:
+        url = movie['link'].replace(' ','%20')
+        name = movie['name'].replace('\t',' ')
+        name = name.replace('Script','')
 
-pprint(data)
+        print(url)
+        html = request.urlopen(url).read().decode('utf8',errors='ignore')
+        soup = BeautifulSoup(html,'html.parser')
+        links_in_page = soup.find_all('a')
+        found = False
+        for link in links_in_page:
+            text = link.get_text()
+            print("->" + text)
+            if 'Read' in text and 'Script' in text:
+                found = True
+                script_link = 'http://www.imsdb.com' + link.get('href')
+                print(name + '\t' + script_link)
+                out.write(name + '\t' + script_link + '\n')
+                break
+        if found == False:
+             print('NOTFOUND',name + '\t' + script_link)
